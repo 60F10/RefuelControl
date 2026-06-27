4d7c2d897c8de7935f7cb3a70baba35eb30d7652
60F10 <raulpjk97@gmail.com>
2026-06-27 02:13:27 +0100

    Add main functionality for processing fuel tickets and updating dashboard

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'repostajes-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.add('index.html')));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
